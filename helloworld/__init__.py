8be5d0669e5dadab579b7f1783b462614a2bf4b5
Kyle Vitale <53164742+KV-CS@users.noreply.github.com>
2024-08-26 07:31:23 -0700

    Update __init__.py

--- helloworld/__init__.py
+++ helloworld/__init__.py
@@ -1,6 +1,10 @@
 import check50
 
 @check50.check()
-def exists():
+def hw_exists():
     """helloworld.py exists."""
     check50.exists("helloworld.py")
+
+def hn_exists():
+    """helloname.py exists."""
+    check50.exists("helloname.py")
