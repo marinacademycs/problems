45b1172c12c3e4f8bb50859484f37f8d7d8f4b74
Kyle Vitale <53164742+KV-CS@users.noreply.github.com>
2024-10-08 14:31:07 -0700

    Update __init__.py

--- bank/__init__.py
+++ bank/__init__.py
@@ -60,7 +60,7 @@ def testWhats_up():
 def testNewman_Hello():
     """input of \"Newman, hello?\" yields output of $0"""
     input = "Newman, hello?"
-    output = "$0"
+    output = "$100"
     check50.run("python3 bank.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
 
 
